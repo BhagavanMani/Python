be06a94ab058ef70a01d1e138fdaa11f0867cbb3
BhagavanMani <33350543+BhagavanMani@users.noreply.github.com>
2018-01-05 22:33:59 +0530

    Add files via upload

---
+++ 1_IntegerTypes.py
@@ -0,0 +1,27 @@
+#Python Datatypes 
+Num1=4.2
+Num2=4.6
+Num3=4.5
+Num4=-4.2
+Num5=-4.6
+Num6=-4.5
+
+from math import floor,ceil
+print round(Num1)
+# print round(Num2)
+# print round(Num3)
+# print round(Num4)
+# print round(Num5)
+# print round(Num6)
+# print floor(Num1)
+# print floor(Num2)
+# print floor(Num3)
+# print floor(Num4)
+# print floor(Num5)
+# print ceil(Num1)
+# print ceil(Num2)
+# print ceil(Num3)
+# print ceil(Num4)
+# print ceil(Num5)
+# print ceil(Num6)
+
