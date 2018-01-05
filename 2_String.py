4e91c04f56ea93cdcc7493b8acc80bd1a2bd8e40
BhagavanMani <33350543+BhagavanMani@users.noreply.github.com>
2018-01-05 22:34:27 +0530

    Add files via upload

---
+++ 2_String.py
@@ -0,0 +1,73 @@
+# print (type ("abc"))
+# print (type (u"abc"))
+# print (chr(97))
+# print (ord('A'))
+
+''' \b removes one char before
+ \t for tab space
+ \" for double quote in a string
+ \' for single quote in a string
+ \\ for \ in a string '''
+
+# print ("Bhagavan\bMani")
+# print ('Bhagavan\tMani')
+# print ('Bhagavan\"Mani')
+# print ('Bhagavan\'Mani')
+# print ('Bhagavan\\Mani')
+
+str="BhagavanMani"
+# print (str.center(25))
+# print (len (str))
+# print (len(str.center(25)))
+# print (str.center(5))
+# print (len(str.center(5)))
+
+# print str.rjust(50)
+# print str.ljust(50)
+# strTab='     '+str+'        ' #5+str+8
+# print strTab
+# print len(strTab)
+# print strTab.rstrip()
+# print strTab.lstrip()
+# print len(strTab.rstrip())
+# print len(strTab.lstrip())
+# print strTab.strip()
+# print len(strTab.strip())
+
+# i=10
+# print str.count('a')#always takes string as variable
+# print str.count(i)
+
+
+# print str.find('a')
+# print str.rfind('a')
+
+
+# t=('i',)
+# print type(t)
+# print str.startswith('B')
+# print str.startswith('b')
+# print str.startswith('M')
+# print str.endswith(u'B')
+# print str.endswith(t)
+
+# k=123
+# print str.lower()
+# print str.upper()
+# print k.upper()
+
+str1='Bhagavan143Mani'
+# k='1234'
+# print str.isalpha()
+# print str1.isalpha()
+# print str.isupper()
+# print str.islower()
+# print (str1.upper()).isupper()
+# print (str.lower()).islower()
+# print k.isdigit()
+
+# line=" 1.4 ,8.6 ,-3.30 4.29, 124"
+# print line.split()
+# print line.split(',')
+# print line.split(',',1)
+# print line.split(',',2)
