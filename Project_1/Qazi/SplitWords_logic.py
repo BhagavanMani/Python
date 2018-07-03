5677e456768bf845621aeb0cc9c91b11dd205d32
BhagavanMani <33350543+BhagavanMani@users.noreply.github.com>
2018-07-03 12:08:14 +0530

    Update SplitWords_logic.py

--- Project_1/Qazi/SplitWords_logic.py
+++ Project_1/Qazi/SplitWords_logic.py
@@ -16,24 +16,30 @@ stn='Bhagavan | CenduitServices | Sr Software Engineer | Bhagavan | CenduitServi
 # name=str[:ind]
 # print(name)
 
+def splitToDiffWords(str, delm, pos=0):
+    ''' This function will skip the delimiter value'''
+    temp = 0
+    count = str.count(delm)
+    lst = []
+    if pos > 0:
+        str = str[pos:]
+    if delm in str:
+        for i in range(count + 1):
+            if temp == count:
+                ind = len(str) + len(delm)
+            else:
+                ind = str.index(delm)
+            name = str[:ind]
+            str = str[ind + len(delm):].strip()
+            if name.strip()!='':
+              lst.append(name)
+            temp = temp + 1
+        return lst
+    else:
+        return 'Passed delimiter is not present in the given string'
 
-def splitToDiffWords(str,delm,pos=0):
-    temp=0
-    count=str.count(delm)
-    lst=[]
-    if pos>0:
-        str=str[pos:]
-    for i in range(count+1):
-        if temp==count:
-            ind=len(str)+1
-        else:
-            ind=str.index(delm)
-        name=str[:ind]
-        str=str[ind+1:].strip()
-        lst.append(name)
-        temp=temp+1
-    return lst
-print(splitToDiffWords(stn,'|',5))
-
+stn = 'Bhagavan | Cenduit | Sr Software Engineer | Bhoom | Bhoommm | Bhoooommmmmmm...!!'
+# print(splitToDiffWords(stn, '|'))
+print(splitToDiffWords(stn, 'om'))
 
 
